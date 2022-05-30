c48c3e602722a9c9c1de9cb2dadf67194d88bad7
Sachindu Peramuna <sachinduayodye@gmail.com>
2022-05-30 20:23:07 +0530

    Add new packages

--- src/main/java/com/example/groceryspring/GrocerySpringApplication.java
+++ src/main/java/com/example/groceryspring/GrocerySpringApplication.java
@@ -1,13 +1,25 @@
 package com.example.groceryspring;
 
+import com.example.groceryspring.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-public class GrocerySpringApplication {
+@EnableMongoRepositories
+public class GrocerySpringApplication implements CommandLineRunner {
+
+    @Autowired
+    ItemRepository groceryItemRepo;
 
     public static void main(String[] args) {
         SpringApplication.run(GrocerySpringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
 }
