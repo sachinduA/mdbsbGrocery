669eb6370db0e0eb20d657fa6900620d00cbf016
Sachindu Peramuna <sachinduayodye@gmail.com>
2022-05-30 19:43:47 +0530

    Create Project structure

---
+++ src/main/java/com/example/groceryspring/GrocerySpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.groceryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrocerySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/groceryspring/GrocerySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groceryspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
